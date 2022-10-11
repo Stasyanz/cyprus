7b9484db218bc3407b15a38d48eb10f9ce6f2183
dev <stasyanzayac@gmail.com>
2022-10-11 10:18:38 +0300

    add Wallet to admin interface

--- balance/admin.py
+++ balance/admin.py
@@ -3,6 +3,11 @@ from django.db.models import Sum, Count, Avg
 from balance.models import Transaction, Wallet
 
 
+class WalletAdmin(admin.ModelAdmin):
+    list_filter = ["address", ]
+    search_fields = ["address", ]
+
+
 class TransactionAdmin(admin.ModelAdmin):
     """Admin site model representation"""
     list_display = ["hash", "ledger_index", "amount", "account", "wallet", "destination_tag", "fee", ]
@@ -10,5 +15,5 @@ class TransactionAdmin(admin.ModelAdmin):
     list_filter = ["wallet", "account", "amount",  "fee", ]
 
 
-admin.site.register(Wallet,)
+admin.site.register(Wallet, WalletAdmin)
 admin.site.register(Transaction, TransactionAdmin)
